04b594a32a4f8a69e5b709f2dbad979762f96451
genri <genri@ETHXXX>
2024-06-12 23:33:55 +0300

    Добавьте файлы проекта.

---
+++ Test/Test.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "..\prak8\Header.h"
+#include "Windows.h"
+
+int main()
+{
+    std::cout << Sum(1, 2);
+}
+
---
+++ Test1/Test1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "..\prak8\header.h"
+#include "Windows.h"
+
+int main()
+{
+	HINSTANCE load;
+	load = LoadLibrary(L"Dynamic.dll");
+	typedef int (*sum) (int, int);
+	sum Sum;
+	Sum = (sum)GetProcAddress(load, "Sum");
+	std::cout << (2, 5);
+	FreeLibrary(load);
+}
